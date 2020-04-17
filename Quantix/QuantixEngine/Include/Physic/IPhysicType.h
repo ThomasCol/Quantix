a613cd349dcb6eea6323ee6e308548a6b27fa098
Baptiste Rimetz <b.rimetz@student.isartdigital.com>
2020-04-18 00:37:41 +0200

    [Add] Start IPhysicType

--- Quantix/QuantixEngine/Include/Physic/IPhysicType.h
+++ Quantix/QuantixEngine/Include/Physic/IPhysicType.h
@@ -1,9 +1,65 @@
-#Ifndef __IPHYSICTYPE_H__
+#ifndef __IPHYSICTYPE_H__
 #define __IPHYSICTYPE_H__
 
-//#include <PxActor.h
+//#include <PxActor.h>
+//#include <PxPhysicsAPI.h>
+//#include <PxRigidActorExt.h>
 
-class IPhysicType
+namespace Physic
 {
-};
+
+	class PhysicStatic;
+	class PhysicDynamic;
+
+	enum class	EPhysXShape
+	{
+		CUBE,
+		SPHERE,
+		CAPSULE
+	};
+
+	enum class ETypePhysic
+	{
+		STATIC,
+		DYNAMIC,
+		NONE
+	};
+
+	class IPhysicType
+	{
+	protected:
+		ETypePhysic		type = ETypePhysic::NONE;
+
+	public:
+		IPhysicType() noexcept;
+		IPhysicType(ETypePhysic oType) noexcept;
+		IPhysicType(const IPhysicType&) noexcept;
+		IPhysicType(IPhysicType&&) noexcept;
+		virtual ~IPhysicType() = default;
+
+		void		print();
+
+		ETypePhysic		GetType();
+		template<class T>
+		T* GetObject()
+		{
+			if (type == ETypePhysic::STATIC)
+				return (dynamic_cast<PhysicStatic*>(this));
+			else if (type == ETypePhysic::DYNAMIC)
+				return (dynamic_cast<PhysicDynamic*>(this));
+		}
+
+		PhysicDynamic* GetObjectDynamic()
+		{
+			return (PhysicDynamic*)this;
+		}
+
+		PhysicStatic* GetObjectStatic()
+		{
+			return (PhysicStatic*)this;
+		}
+	};
+}
+
+#endif
 
--- Quantix/QuantixEngine/Src/Physic/IPhysicType.cpp
+++ Quantix/QuantixEngine/Src/Physic/IPhysicType.cpp
@@ -1,1 +1,38 @@
-//#include "IPhysicType.h"
+#include <iostream>
+
+#include "Physic/IPhysicType.h"
+//#include "Physic/PhysicStatic.h"
+//#include "Physic/PhysicDynamic.h"
+
+namespace Physic
+{
+	IPhysicType::IPhysicType() noexcept :
+		type{ ETypePhysic::NONE }
+	{}
+
+	IPhysicType::IPhysicType(ETypePhysic oType) noexcept :
+		type{ oType }
+	{}
+
+	IPhysicType::IPhysicType(const IPhysicType& Pt) noexcept : 
+		type{ Pt.type }
+	{}
+
+	IPhysicType::IPhysicType(IPhysicType&& pt) noexcept :
+		type {std::move(pt.type)}
+	{}
+
+	void IPhysicType::print()
+	{
+		/*if (type == ETypePhysic::STATIC)
+			(dynamic_cast<PhysicStatic*>(this))->print();
+		else if (type == ETypePhysic::DYNAMIC)
+			(dynamic_cast<PhysicDynamic*>(this))->print();*/
+	}
+
+	ETypePhysic IPhysicType::GetType()
+	{
+		return type;
+	}
+}
+
